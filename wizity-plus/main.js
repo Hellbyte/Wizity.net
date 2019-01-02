c87f1abb12894e47ef24b31cfd2796717adeb892
Hellbyte <hellbyte17@gmail.com>
2019-01-02 18:58:14 +0100

    Update main.js

--- wizity-plus/main.js
+++ wizity-plus/main.js
@@ -1,3 +1,20 @@
 var config = {
-  
+  name: "Wizity+",
+  version: "1.0",
+  status: false,
+  api: "Wizity.net APIv1",
+  site: "https://wizity.net",
+  developers: [
+    1
+  ],
+  functions: {
+    autolike: true,
+    autojoin: false
+  }
 };
+
+// Enable API
+API.enable(config.api, function(){
+  config.status = true;
+  console.log(config.name +" version "+ config.version +" is running..");
+});
