9833f3be63dcf6f32d0efa07d054199020068b30
Hellbyte <hellbyte17@gmail.com>
2019-02-12 14:46:11 +0100

    Update bot.js

--- repi/bot.js
+++ repi/bot.js
@@ -1,3 +1,14 @@
-(function(){
+var bot = {
+  name: "Bot",
+  version: "1.0",
+  room: "url",
+  api: {};
+};
 
+bot.api.onStart = function(){
+  API.sendChat(bot.name +" version "+ bot.version +"is successfully enabled!");
+}
+
+(function(){
+  bot.api.onStart();
 }());
